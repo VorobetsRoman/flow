8f0a060161389caa74d7104fa57f14201ae7875f
Роман Воробец <vorobetsroman@mail.ru>
2023-07-27 20:23:45 +0300

    ready

--- src/ireciver.cpp
+++ src/ireciver.cpp
@@ -7,45 +7,58 @@ Reciver::Reciver(ICallback *callback)
 
 }
 
+Reciver::~Reciver()
+{
+    if (m_cache) {
+        delete [] m_cache;
+    }
+}
+
 void Reciver::Recive(
     const char *data
     , std::size_t size
 ) {
     if (m_cache) {
+        m_increaseCache(size);
+
         memcpy(m_cache + m_cacheSize, data, size);
-        m_cacheSize += size;
-        m_procesData(m_cache, m_cacheSize);
+
+        m_processBlock(m_cache, m_cacheSize);
     } else {
-        m_procesData(data, size);
+        m_processBlock(data, size);
     }
 }
 
-void Reciver::m_procesData(
+void Reciver::m_processBlock(
     const char *data
     , std::size_t size
 ) {
     int readed = 0;
     if (m_isBinary(data)) {
-        if(size > 4 + 1) {
+        if (size > mc_binaryHeaderSize) {
             readed = m_processBinary(data, size);
         }
     } else {
         readed = m_processText(data, size);
     }
-    if (readed == 0) { //данных не хватило
-        if (!m_cache) { //если контейнер не существует
+
+    if (readed == 0) { //блок в потоке не закончился
+        if (!m_cache) { //если контейнер не существует, т.е. читали из потока
             m_cache = new char [size];
             m_cacheSize = size;
+            memcpy(m_cache, data, size);
         } // иначе ждём новых данных
     } else {
         size -= readed;
-        if (size > 0) { //остались данные
+        if (size > 0) { //в потоке остались данные
             if (m_cache) {
                 memmove(m_cache, m_cache + readed, size);
                 m_cacheSize -= size;
+                m_processBlock(m_cache, m_cacheSize);
+            } else {
+                m_processBlock(data + readed, size);
             }
-            m_procesData(data + readed, size);
-        } else {
+        } else {//конец блока совпал с концом потока
             if (m_cache) {
                 delete [] m_cache;
                 m_cache = nullptr;
@@ -61,13 +74,14 @@ int Reciver::m_processBinary(
 ) {
     const u_int32_t *datasize = reinterpret_cast
         <const u_int32_t*>
-        (data + 1);
-    const auto binaryPackageSize = *datasize + 4 + 1;
+        (data + mc_idSize);
+    const auto binaryPackageSize = mc_binaryHeaderSize + *datasize;
     if (size >= binaryPackageSize) {
-        mp_callback->BinaryPacket(data + 1 + 4, *datasize);
+        mp_callback->BinaryPacket(data + mc_binaryHeaderSize, *datasize);
         return binaryPackageSize;
+    } else {
+        return 0;
     }
-    return 0;
 }
 
 int Reciver::m_processText(
@@ -77,15 +91,25 @@ int Reciver::m_processText(
     const char* foundPos = strstr(data, m_finish);
 
     if (foundPos != nullptr) {
-        auto strlen = foundPos - data + 4;
+        auto strlen = foundPos - data + mc_binaryDataSize;
         mp_callback->TextPacket(foundPos, strlen);
         return strlen;
+    } else {
+        return 0;
     }
-    return 0;
 }
 
 bool Reciver::m_isBinary(const char *data)
 {
     const u_int8_t *dataId = reinterpret_cast<const u_int8_t*>(data);
     return *dataId == mc_binaryId;
+}
+
+void Reciver::m_increaseCache(std::size_t appendSize)
+{
+    auto temp = m_cache;
+    m_cache = new char [m_cacheSize + appendSize];
+    memcpy(m_cache, temp, m_cacheSize);
+    m_cacheSize += appendSize;
+    delete [] temp;
 }
--- src/ireciver.h
+++ src/ireciver.h
@@ -13,18 +13,24 @@ class Reciver : public IReciver
 {
 public:
     Reciver(ICallback *callback);
+    ~Reciver();
         // IReciver interface
     void Recive(const char *data, std::size_t size);
 
 
 private:
     ICallback *mp_callback {nullptr};
+
     static const u_int8_t mc_binaryId = 0x24;
+    static constexpr std::size_t mc_idSize = sizeof(mc_binaryId);
+    static const std::size_t mc_binaryDataSize = 4;
+    static constexpr std::size_t mc_binaryHeaderSize = mc_idSize + mc_binaryDataSize;
     const char *m_finish = "\r\n\r\n";
+
     char *m_cache {nullptr};
     std::size_t m_cacheSize {0};
 
-    void m_procesData(
+    void m_processBlock(
         const char* data
         , std::size_t size
     );
@@ -39,6 +45,8 @@ private:
     );
 
     bool m_isBinary(const char *data);
+
+    void m_increaseCache(std::size_t appendSize);
 };
 
 #endif // IRECIVER_H
